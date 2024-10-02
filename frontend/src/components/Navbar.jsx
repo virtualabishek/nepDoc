5549c57b4b2732a82a74e5d00561d3595c333a61
virtualabishek <717abishekneupane@gmail.com>
2024-10-02 19:13:35 +0545

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+      <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
+      <ul className="hidden md:flex items-start gap-5 font-medium">
+        <NavLink>
+          <li>HOME</li>
+          <hr />
+        </NavLink>
+        <NavLink>
+          <li>ALL DOCTORS</li>
+          <hr />
+        </NavLink>
+        <NavLink>
+          <li>ABOUT</li>
+          <hr />
+        </NavLink>
+        <NavLink>
+          <li>CONTACT</li>
+          <hr />
+        </NavLink>
+      </ul>
+      <div>
+        <button>Create Account</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
