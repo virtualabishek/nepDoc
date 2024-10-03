54952409f210fa09b181738e323f5f0b8b992a65
virtualabishek <717abishekneupane@gmail.com>
2024-10-03 16:35:42 +0545

    done for afternoon session

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,27 +3,45 @@ import { assets } from "../assets/assets";
 
 const Header = () => {
   return (
-    <div className="flex flex-col md:flex">
-      {/* for left side */}
-      <div>
-        <p>
-          Book Appointmnet <br /> With Trusted Doctors{" "}
+    <div className="flex flex-col md:flex-row bg-primary rounded-lg px-6 md:px-10 lg:px-20 relative">
+      {/* Left Side */}
+      <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 md:py-[10vw] md:mb-[-30px]">
+        {/* Heading */}
+        <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
+          Book Appointment <br /> With Trusted Doctors
         </p>
+
+        {/* Profile and Text */}
+        <div className="flex items-center gap-3 text-white text-sm font-light">
+          <img
+            className="w-24 rounded-full"
+            src={assets.group_profiles}
+            alt="Profiles"
+          />
+          <p className="font-semibold">
+            Simply browse through our extensive list of trusted doctors,
+            <br className="hidden sm:block" /> schedule your appointment
+            hassle-free.
+          </p>
+        </div>
+
+        {/* Book Appointment Button */}
+        <a
+          className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm hover:scale-105 transition-all duration-300"
+          href="#"
+        >
+          Book appointment{" "}
+          <img className="w-3" src={assets.arrow_icon} alt="Arrow" />
+        </a>
       </div>
-      {/* for right side */}
-      <div>
-        <img className="w-20" src={assets.group_profiles} alt="" />
-        <p>
-          Simply browse through our extensive list of trusted doctors,
-          <br /> schedule your appointment hassle-free.
-        </p>
-      </div>
-      <a href="">
-        Book appointment <img src={assets.arrow_icon} alt="" />
-      </a>
-      {/* Right SIde Contenet */}
-      <div>
-        <img src={assets.header_img} alt="" />
+
+      {/* Right Side Image */}
+      <div className="md:w-1/2 relative">
+        <img
+          className="w-full md:absolute bottom-0 right-0 h-auto rounded-lg"
+          src={assets.header_img}
+          alt="Doctors"
+        />
       </div>
     </div>
   );
