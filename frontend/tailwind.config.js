2c5a3c3de370d35abc5631d49197007b2af85001
virtualabishek <717abishekneupane@gmail.com>
2024-10-04 09:30:40 +0545

    Done for now. Added Banner Section, Top Doctor Section with navigation function and context

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       colors: {
         primary: "#5f6fff",
       },
+      gridTemplateColumns: {
+        auto: "repeat(auto-fill, minmax(200px, 1fr))",
+      },
     },
   },
   plugins: [],
