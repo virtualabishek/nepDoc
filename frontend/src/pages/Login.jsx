032b0c24190647d9cfa4169ad78641438504e2d9
virtualabishek <717abishekneupane@gmail.com>
2024-10-07 09:02:23 +0545

    Solved yesterday error and succesfully created a Sign In / Sign Up Page using state on the same Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div></div>;
+  const [state, setState] = useState("Sign Up");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const onSubmithandler = async (event) => {
+    event.preventDefualt();
+  };
+  return (
+    <div className="min-h-[80vh] flex items-center">
+      <form className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
+        <p className="text-2xl font-semibold">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </p>
+        <p>
+          Please {state === "Sign Up" ? "Sign Up" : "Log In"} to book
+          Appointment.
+        </p>
+        {state === "Sign Up" && (
+          <div className="w-full">
+            <p>Full Name:</p>
+            <input
+              className=" border border-zinx-300 rounded w-full p-2 mt-1"
+              type="text"
+              onChange={() => {
+                (e) => setName(e.target.name);
+              }}
+              value={name}
+              required
+            />
+          </div>
+        )}
+
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            className=" border border-zinx-300 rounded w-full p-2 mt-1"
+            type="email"
+            onChange={() => {
+              (e) => setEmail(e.target.email);
+            }}
+            value={name}
+            required
+          />
+        </div>
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className=" border border-zinx-300 rounded w-full p-2 mt-1"
+            type="password"
+            onChange={() => {
+              (e) => setPassword(e.target.password);
+            }}
+            value={name}
+            required
+          />
+        </div>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+        {state === "Sign Up" ? (
+          <p>
+            Already have an account?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className="text-primary underline cursor-pointer"
+            >
+              Login here
+            </span>{" "}
+          </p>
+        ) : (
+          <p>
+            Create a new account?{" "}
+            <span
+              onClick={() => setState("Sign Up")}
+              className="text-primary underline cursor-pointer"
+            >
+              Click Here
+            </span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
