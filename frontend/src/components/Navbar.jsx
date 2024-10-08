dce630632409bbd2b515106e0ff8469004003e7a
virtualabishek <717abishekneupane@gmail.com>
2024-10-08 08:29:06 +0545

    Finished all the stuffs. Trying to make it more responsiveness on small devices.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -72,6 +72,42 @@ const Navbar = () => {
             Create Account
           </button>
         )}
+        <img
+          onClick={() => setShowMenu(true)}
+          className="w-6 md:hidden "
+          src={assets.menu_icon}
+          alt=""
+        />
+        {/* ________Mobile menu _____________ */}
+        <div
+          className={`${
+            showMenu ? "fixed w-full" : "h-0 w-0"
+          }md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}
+        >
+          <div className="flex items-center justify-between px-5 py-6">
+            <img className="w-36" src={assets.logo} alt="" />
+            <img
+              className="w-7"
+              onClick={() => setShowMenu(false)}
+              src={assets.cross_icon}
+              alt=""
+            />
+            <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+              <NavLink onClick={() => setShowMenu(false)} to="/">
+                <p className="px-4 py-2 rounded inline-block">ALL DOCTORS</p>
+              </NavLink>
+              <NavLink onClick={() => setShowMenu(false)} to="/doctors">
+                <p className="px-4 py-2 rounded inline-block">ALL DOCTORS</p>
+              </NavLink>
+              <NavLink onClick={() => setShowMenu(false)} to="/about">
+                <p className="px-4 py-2 rounded inline-block">ABOUT</p>
+              </NavLink>
+              <NavLink onClick={() => setShowMenu(false)} to="/contact">
+                <p className="px-4 py-2 rounded inline-block">CONTACT</p>
+              </NavLink>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
