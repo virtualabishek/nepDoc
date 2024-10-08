029636bbaee60a497ab2378ab2516bf23f1d56f7
virtualabishek <717abishekneupane@gmail.com>
2024-10-08 07:53:23 +0545

    Creating the appointment page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/my-profile" element={<MyProfile />} />
-        <Route path="/my-appointmnets" element={<MyAppointments />} />
+        <Route path="/my-appointment" element={<MyAppointments />} />
         <Route path="/appointment/:docId" element={<Appointment />} />
       </Routes>
       <Footer />
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const MyAppointments = () => {
-  return <div></div>;
+  const { doctors } = useContext(AppContext);
+  return (
+    <div>
+      <p className="pb-3 mb-12 font-medium text-zinc-700 border-b">
+        My Appointments
+      </p>
+      <div>
+        {doctors.slice(0, 2).map((item, index) => (
+          <div key={index}>
+            <div>
+              <img src={item.image} alt="" />
+            </div>
+            <div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p>Address:</p>
+              <p>{item.address.line1}</p>
+              <p>{item.address.line2}</p>
+              <p>
+                <span>Date & Time:</span>19, October, 2024 | 8: 30 am
+              </p>
+            </div>
+            <div></div>
+            <div>
+              <button>Pay Online</button>
+              <button>Cancel Appoitment</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
