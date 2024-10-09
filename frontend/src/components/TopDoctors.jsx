61a7b6781115c6d2dd8e4afbe4ac7e4a34199327
virtualabishek <717abishekneupane@gmail.com>
2024-10-09 15:45:10 +0545

    done. corrected the error on the top doctor pages

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -37,7 +37,7 @@ const TopDoctors = () => {
       </div>
       <button
         onClick={() => {
-          navigate("/doctos");
+          navigate("/doctors");
           scrollTo(0, 0);
         }}
         className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10"
